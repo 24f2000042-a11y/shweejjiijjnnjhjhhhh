7d5605a265c86060833223092c85d6aceb6bf746
24f2000042-a11y <24f2000042@ds.study.iitm.ac.in>
2026-05-20 22:51:24 +0530

    add backend/middleware/authMiddleware.js

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+module.exports = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) return res.status(403).send('Forbidden');
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).send('Forbidden');
+        req.user = user;
+        next();
+    });
+};
